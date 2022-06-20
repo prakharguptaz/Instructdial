406d443ddfb55e0b04ea63417f40d9756d5b49fc
Alon Albalak <alonalbalak@gmail.com>
2022-06-20 14:57:04 -0700

    fix broken smcalflow link

--- datasets/smcalflow.sh
+++ datasets/smcalflow.sh
@@ -1,2 +1,2 @@
-wget -P ./smcalflow https://github.com/microsoft/task_oriented_dialogue_as_dataflow_synthesis/blob/master/datasets/SMCalFlow%202.0/train.dataflow_dialogues.jsonl.zip
+wget -P ./smcalflow https://github.com/microsoft/task_oriented_dialogue_as_dataflow_synthesis/raw/master/datasets/SMCalFlow%202.0/train.dataflow_dialogues.jsonl.zip
 unzip ./smcalflow/train.dataflow_dialogues.jsonl.zip -d ./smcalflow
