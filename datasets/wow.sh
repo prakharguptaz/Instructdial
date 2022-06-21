a429b361dd58964a7ccb1c3a9ceedcf304d60e36
exe1023 <yitingye@cs.cmu.edu>
2022-06-21 15:28:59 -0400

    fix wow process error and missing dependency

--- datasets/wow.sh
+++ datasets/wow.sh
@@ -1,4 +1,8 @@
 wget -P ./ -nc http://parl.ai/downloads/wizard_of_wikipedia/wizard_of_wikipedia.tgz
-tar --keep-old-files  -xvzf ./wizard_of_wikipedia.tgz
+tar  -xvzf ./wizard_of_wikipedia.tgz -C WoW 
+
+cd WoW
 python ./wizard_generator.py
 printf '%s\n' "${PWD##*/}"
+
+cd ../
